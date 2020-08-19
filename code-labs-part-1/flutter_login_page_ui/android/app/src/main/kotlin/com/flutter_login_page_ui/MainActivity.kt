bdd16586f85cf8a07b5c1c40a3fc9d0fd0252045
MICHAL SOBIECKI <kontakt@codeshaker.pl>
2020-08-19 19:13:17 +0200

    Added login_page_ui

---
+++ code-labs-part-1/flutter_login_page_ui/android/app/src/main/kotlin/com/flutter_login_page_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter_login_page_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ code-labs-part-1/flutter_login_page_ui/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_page_ui/screens/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
